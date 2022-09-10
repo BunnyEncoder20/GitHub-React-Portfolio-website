de12509679a5361711dedba4825f87fe91aa4834
BunnyEncoder20 <103634057+BunnyEncoder20@users.noreply.github.com>
2022-09-10 07:06:27 +0530

    tried to add some corrections, no luck
    the code is still not showing anything on the webpage and it is still blank

--- portfolio-website-1/src/App.jsx
+++ portfolio-website-1/src/App.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 
-import header from './components/header/header'
-import Nav from './components/nav/nav'
-import about from './components/about/about'
-import experience from './components/experience/experience'
-import services from './components/services/services'
-import portfolio from './components/portfolio/portfolio'
-import testimonials from './components/testimonials/testimonials'
-import contact from './components/contact/contact'
-import footer from './components/footer/footer'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 
 const App = () => {
   return (
     <>
-        <header/>
-        <nav />
-        <about />
-        <experience />
-        <services />
-        <portfolio />
-        <testimonials />
-        <contact />
-        <footer />
+      <div>Hello?</div>
+        <Header/>
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
     </>
   )
 }
