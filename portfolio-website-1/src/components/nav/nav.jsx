fc6cf0852fe71a409f264594abbe95887559e445
Saketh0610 <110999583+Saketh0610@users.noreply.github.com>
2022-10-19 00:56:17 +0530

    Update nav.jsx

--- portfolio-website-1/src/components/nav/nav.jsx
+++ portfolio-website-1/src/components/nav/nav.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import "./nav.css"
-
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icon/ri'
+import {RiServiceLine} from 'react-icon/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import {useState} from 'react'
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
+    const [activeNav, setActiveNav] = useState('#')
+    return (
+        <nav>
+            <a href="#" onClick={() => setActivateNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+            <a href="#experience" onClick={() => setActiveNav()} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+            <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+        </nav>
+    )
 }
-
 export default Nav
